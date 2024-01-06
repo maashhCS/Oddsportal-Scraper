e3ebe495b33adc5c5b15f5fa782daa536892bd7f
maashhCS <philipp.burgstaller@yandex.com>
2024-01-06 16:23:31 +0100

    cleaned up the code

--- Program.cs
+++ Program.cs
@@ -9,13 +9,61 @@ internal class Program
     {
         var scraper = new Scraper();
         var url = "https://www.oddsportal.com/matches/football/";
-        await scraper.GetData(url);
+        var infos = await scraper.GetData(url);
+        foreach (var match in infos)
+        {
+            Console.Write($"{match.HomeTeam} ");
+            if (match.HomeTeamScore > match.AwayTeamScore)
+            {
+                WriteToConsole(match.HomeTeamScore, match.AwayTeamScore, ConsoleColor.Green, ConsoleColor.Red);
+            }
+            else if (match.HomeTeamScore == match.AwayTeamScore)
+            {
+                WriteToConsole(match.HomeTeamScore, match.AwayTeamScore, ConsoleColor.Yellow, ConsoleColor.Yellow);
+            }
+            else
+            {
+                WriteToConsole(match.HomeTeamScore, match.AwayTeamScore, ConsoleColor.Red, ConsoleColor.Green);
+            }
+            Console.Write($"{match.AwayTeam}\n");
+        }
+
+        var test = infos.LastOrDefault();
+    }
+
+    private static void WriteToConsole(int? homeScore, int? awayScore, ConsoleColor homeColor, ConsoleColor awayColor)
+    {
+        if (homeScore != null)
+        {
+            Console.ForegroundColor = homeColor;
+            Console.Write($"{homeScore} ");
+            Console.ResetColor();
+        }
+
+        Console.Write("- ");
+
+        if (awayScore != null)
+        {
+            Console.ForegroundColor = awayColor;
+            Console.Write($"{awayScore} ");
+            Console.ResetColor();
+        }
     }
 }
 
 public class Scraper
 {
-    public async Task GetData(string url)
+    public async Task<List<MatchInfos>> GetData(string url)
+    {
+        var browser = await LaunchBrowser();
+        var page = await browser.NewPageAsync();
+        page = await OpenUrl(page, url);
+        var matchInfos = await ExtractMatchInfos(page);
+        await browser.CloseAsync();
+        return matchInfos;
+    }
+
+    private async Task<IBrowser> LaunchBrowser()
     {
         using var browserFetcher = new BrowserFetcher();
         await browserFetcher.DownloadAsync();
@@ -24,7 +72,11 @@ public class Scraper
             Headless = false,
             Args = new[] { "--start-maximized" }
         });
-        var page = await browser.NewPageAsync();
+        return browser;
+    }
+
+    private async Task<IPage> OpenUrl(IPage page, string url)
+    {
         await page.SetViewportAsync(new ViewPortOptions { Width = 1920, Height = 1080, DeviceScaleFactor = 1 });
         await page.GoToAsync(url);
         await Task.Delay(2000);
@@ -33,17 +85,39 @@ public class Scraper
         {
             Button = MouseButton.Left
         });
+        await ScrollToBottom(page);
+        // var htmlLength = await page.MainFrame.GetContentAsync();
+        // string lengthBefore;
+        // do
+        // {
+        //     lengthBefore = htmlLength;
+        //     await page.EvaluateExpressionAsync("window.scrollBy(0, 20000)");
+        //     await Task.Delay(2000);
+        //     htmlLength = await page.MainFrame.GetContentAsync();
+        // } while (htmlLength.Length > lengthBefore.Length);
+        return page;
+    }
+    
+    private async Task ScrollToBottom(IPage page)
+    {
+        string script = @"(async () => {
+                            const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+                            while (true) {
+                                const lastHeight = document.body.scrollHeight;
+                                window.scrollTo({ top: lastHeight, behavior: 'smooth' });
+                                await delay(1000);
+                                const newHeight = document.body.scrollHeight;
+                                if (newHeight === lastHeight) {
+                                    break;
+                                }
+                            }
+                        })();";
 
-        var htmlLength = await page.MainFrame.GetContentAsync();
-        string lengthBefore;
-        do
-        {
-            lengthBefore = htmlLength;
-            await page.EvaluateExpressionAsync("window.scrollBy(0, 20000)");
-            await Task.Delay(2000);
-            htmlLength = await page.MainFrame.GetContentAsync();
-        } while (htmlLength.Length > lengthBefore.Length);
+        await page.EvaluateExpressionAsync(script);
+    }
 
+    private async Task<List<MatchInfos>> ExtractMatchInfos(IPage page)
+    {
         var matchDiv = await page.MainFrame.QuerySelectorAsync(
             @"#app > div > div.w-full.flex-center.bg-gray-med_light > div > main > div.relative.w-full.flex-grow-1.min-w-\[320px\].bg-white-main > div.tabs.min-md\:\!mx-\[10px\] > div:nth-child(4) > div:nth-child(1)");
         var matchDivs = await matchDiv.QuerySelectorAllAsync("div > div[id]");
@@ -66,8 +140,8 @@ public class Scraper
             {
                 matchInfosList.Add(matchinfo = new MatchInfos
                 {
-                    HomeTeam = home,
-                    AwayTeam = away
+                    HomeTeam = home.Trim(),
+                    AwayTeam = away.Trim()
                 });
             }
 
@@ -81,24 +155,40 @@ public class Scraper
 
             var HomeScore = await teamScores[0].GetPropertyAsync("innerText");
             var AwayScore = await teamScores[1].GetPropertyAsync("innerText");
-            var homeScore = Convert.ToInt32(await HomeScore.JsonValueAsync());
-            var awayScore = Convert.ToInt32(await AwayScore.JsonValueAsync());
 
-            if (homeScore is int homeGoals && awayScore is int awayGoals)
+            var homeScoreText = await HomeScore.JsonValueAsync();
+            var awayScoreText = await AwayScore.JsonValueAsync();
+
+            if (homeScoreText is string homeScoreText2)
             {
-                matchinfo.HomeTeamScore = homeGoals;
-                matchinfo.AwayTeamScore = awayGoals;
+                if (string.IsNullOrEmpty(homeScoreText2))
+                {
+                    matchinfo.HomeTeamScore = null;
+                }
+                else
+                {
+                    matchinfo.HomeTeamScore = Convert.ToInt32(homeScoreText2);
+                }
+            }
+
+            if (awayScoreText is string awayScoreText2)
+            {
+                if (string.IsNullOrEmpty(awayScoreText2))
+                {
+                    matchinfo.AwayTeamScore = null;
+                }
+                else
+                {
+                    matchinfo.AwayTeamScore = Convert.ToInt32(awayScoreText2);
+                }
             }
         }
 
-        foreach (var match in matchInfosList)
-        {
-            Console.WriteLine($"{match.HomeTeam} {match.HomeTeamScore} - {match.AwayTeamScore} {match.AwayTeam}");
-        }
+        return matchInfosList;
     }
 }
 
-internal class MatchInfos
+public class MatchInfos
 {
     public string HomeTeam { get; set; }
     public int? HomeTeamScore { get; set; }
