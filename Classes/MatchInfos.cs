f18e23868a9d7c7fdd1abcb9274c6b285f70768a
maashh <maashhcsgo@yandex.com>
2024-02-23 12:37:50 +0100

    updated the structure of the project

---
+++ Classes/MatchInfos.cs
@@ -0,0 +1,12 @@
+﻿namespace Oddsportal_Scraper.Classes;
+
+public class MatchInfos
+{
+    public string HomeTeam { get; set; }
+    public int? HomeTeamScore { get; set; }
+    public string AwayTeam { get; set; }
+    public int? AwayTeamScore { get; set; }
+    public bool IsHalfTime { get; set; }
+    public TimeSpan? KickOff { get; set; }
+    public TimeSpan? LiveMinutes { get; set; }
+}
